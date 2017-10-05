04027f0fceea456f40f82445ed1d81b520e46d4b
edson <edbdelima@gmail.com>
2017-10-05 16:00:20 -0300

    Adding pycountry routines for abbreviations and contains location function

--- python_stuff/linkedgazetteer/log_access.py
+++ python_stuff/linkedgazetteer/log_access.py
@@ -1,12 +1,48 @@
 '''Warning: use NumPy for improve loop statements'''
 
+'''Obs.: Atraves do serviço urlLocationsByEntity no LoG
+observa-se que para cada tripla <s, p, o> em que 's' ou 'p' são dos tipos
+(pessoa, local), a maior parte das 
+entidades (s ou p) não contém como resposta
+o local contido na tripla. 
 
-import requests, json, re, unicodedata
+Isso indica que o gazetteer carece de informações relevantes encontradas 
+durante a extração da relacionamentos nos artigos da Wikipedia
+
+Ex.: <Susan Crown, be philanthropist of, Chicago>
+
+Acessando o serviço urlLocationsByEntity com a entidade
+Susan Crown como parâmetro, nenhum local é retornado como resposta. O que
+indica que esse relacionamento pode ser incorporado na base
+de dados como pontencial candidato.
+
+
+'''
+
+import requests, json, re, unicodedata, pycountry
 from collections import Counter
 from urllib.parse import unquote
 from samples.database import pymysql_connect
 
 # -*- coding: utf-8 -*- 
+
+def all_countries():
+	return list(pycountry.countries)
+
+'''Return specific country name according your abbreviation name'''
+def specific_country(country):
+	try:
+		a = pycountry.countries.get(alpha_2=country)
+		#print('abbreviated')
+		return a.name
+	except:
+		try:
+			a = pycountry.countries.get(alpha_3=country)
+			#print('abbreviated')
+			return a.name
+		except:
+			#print('non abbreviated')
+			return country
 
 '''Round points function from a list of points (coordenates)'''
 def roundPoints(points):
@@ -66,16 +102,25 @@ def get_column_from_result(results):
 def most_freq_value(list):
 	return Counter(list).most_common()
 
+'''Verify if a locations list contains a specific location name (abbreviated or not)'''
+def containsLocation(locations, l):
+	# Too verify if l is a abbriviated name and return the name
+	print(l)
+	s = specific_country(l)
+	print(s.replace(" ", "_"))
+	return True if (l in locations) else False
+
 
 def place_from_name(triples, url):
 	Lp = []
 	for t in triples:
 		l, e = getPlaceNomPlaceEntities(t)
-		urlTemp = url + formatString(l)
+		urlTemp = url + formatString(e)
 		print(urlTemp)
 		La = get_data(urlTemp)
-
-		print (most_freq_value(get_column_from_result(La)))
+		Lb = get_column_from_result(La)
+		if (containsLocation(Lb, l)):
+			print("Opa contains")
 
 # Funcao que percorre a lista de adjacencia a encontra os
 # lugares que casam com o nome passado
@@ -86,7 +131,7 @@ def place_from_name(triples, url):
 baseUrl = "http://sandwich.lbd.dcc.ufmg.br:8080/linkedOntoGazetteerWeb/"
 
 # Busca todos os lugares que estao associados ao nome passado
-# como parametro e os lugares pertencentes à lista de adjacência 
+# como parametro e os lugares pertencentes a lista de adjacencia 
 # dele
 apiServicePlaceAdjacentListByName = "api/place/name/adjacentList/"
 
@@ -94,23 +139,34 @@ apiServicePlaceAdjacentListByName = "api/place/name/adjacentList/"
 apiServiceNamesByPlaceId = "api/name/place/"
 
 # Busca por todos os lugares que 
-# estão associados ao nome informado
+# estao associados ao nome informado
 apiServicePlacesByName = "api/place/name/"
 
 
 # Busca todas todos os lugares
-# que estão relacionadas à entidade (entityName = placeName)
+# que estao relacionadas a entidade (entityName = placeName)
 apiServiceLocation = "api/place/entity/name/"
 
 
-# Busca todas as entidades não classificadas como lugar
-# que estão relacionadas ao lugar (placeId)
+# Busca todas as entidades nao classificadas como lugar
+# que estao relacionadas ao lugar (placeId)
 apiServiceEntity = "api/entity/relatedPlace/"
 
 # Nomes e PlaceIds
 placeName = "US"
-placeId = "9682956"
-entityName = "Edward h"
+placeId = "72178544"
+
+# 1640
+
+# 463356
+
+# 72178544
+
+# 1640
+
+# 41440
+
+entityName = "Barack Obama"
 
 counter = 0
 
@@ -128,19 +184,20 @@ numSeatOfFirstAdmDivision = 0
 
 
 
-# URLs por serviços
+# URLs por servicos
 url = baseUrl + apiServiceEntity + entityName
 urlNamesByPlaceId = baseUrl + apiServiceNamesByPlaceId + placeId
 urlNamesByPlaceName = baseUrl + apiServicePlacesByName
-urlLocationsByEntity = baseUrl + apiServiceLocation + entityName
+urlLocationsByEntity = baseUrl + apiServiceLocation
 urlEntitiesByLocation = baseUrl + apiServiceEntity + placeId
 
 
 # Database access to select triples
 data = pymysql_connect.database_connection()
 
-place_from_name(data, urlNamesByPlaceName)
+place_from_name(data, urlLocationsByEntity)
 
+#print(all_countries())
 
 #response = requests.get(urlLocationsByEntity)
 #response = requests.get(urlNamesByPlaceId)
